40c4622c46bdddce5c4fa61f2b8e35c9b4084f51
Vincent Hsu <vincesuuu1@gmail.com>
2021-09-05 18:41:35 -0700

    Rewrote the keyboard structure with divs in index.html, temporary

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
+        
+    }
+}
