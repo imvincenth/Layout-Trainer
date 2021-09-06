3e65a8f210df5df1abe23a0d00be59f44e3b9747
Vincent Hsu <vincesuuu1@gmail.com>
2021-09-05 19:09:52 -0700

    Styling for the keyboard keys test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 const Keyboard = {
-    elements: {
-        main: null,
-        keysContainer: null,
-        keys: []
-    },
-
-    properties: {
-        value: "",
-        capsLock: false
-    },
-
-    init() {
-        
-    }
+    
 }
