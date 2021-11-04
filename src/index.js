41f35d33ceaa3ff9f6e01f5758127883a2ff2609
Vincent Hsu <vincesuuu1@gmail.com>
2021-11-04 13:49:20 -0700

    Removed console logs, restyled instructions

--- src/index.js
+++ src/index.js
@@ -166,7 +166,7 @@ window.addEventListener("DOMContentLoaded", function () {
     })
 
     window.addEventListener("keydown", function(e) {
-        console.log(e.code)
+
         for (let i = 0; i < keys.length; i++) {
             if (e.key === keys[i].getAttribute("keyname") || e.key === keys[i].getAttribute('lowerCaseName')) {
                 keys[i].classList.add("active");
